e6a45b0048b2c4ec904abf35b2127917fc9702b2
ShawandaBrooks <74687197+ShawandaBrooks@users.noreply.github.com>
2020-12-14 13:06:08 -0500

    added image in about section.

--- js/main.js
+++ js/main.js
@@ -133,4 +133,46 @@ if (glide)
 /*conatct form validation*/
 function validation(){
   var name = document.getElementById("name").value;
+  var subject = document.getElementById("subject").value;
+  var phone = document.getElementById("phone").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  var error_message = document.getElementById("error_message");
+  var text;
+
+  error_message.style.padding = "10px";
+
+  if(name.length < 5){
+    text = "Please enter a valid name";
+    error_message.innerHTML = text;
+    return false;
+  }
+
+  if(subject.length < 2){
+    text = "Please enter subject";
+    error_message.innerHTML = text;
+    return false;
+  }
+
+  if(isNaN(phone) || phone.length != 10){
+    text = "Please enter a valid phone number";
+    error_message.innerHTML = text;
+    return false;
+  }
+
+  if(email.indexOf("@") == -1 || email.length < 6) {
+    text = "Please enter a valid email";
+    error_message.innerHTML = text;
+    return false;
+  }
+
+  if(message.length <= 20){
+    text ="Please be more specific";
+    error_message.innerHTML = text;
+    return false;
+  }
+
+  alert("Form submitted successfully!")
+  return true;
+
 }
